884b3110eddce248b8094c9c57725b5fec9e1804
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 15:43:13 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name); // concatenation
+        Console.WriteLine("Hello {0}", name);// Placeholder syntax with index
+        Console.WriteLine($"Hello {name}");// Placeholder syntax
+    }
+}
+        }
+    
+
